d91cb4cdc1f05117e2078dbfc611fade797389ed
Emmanuel <e.o.donate@gmail.com>
2019-01-11 16:35:53 -0800

    First commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//const Index = () => (
+//    <div>
+//      <p>Hello Next.js</p>
+//    </div>
+//  )
+
+  class Index extends React.Component {
+  render() {
+    return(
+      <div>Hello</div>
+    )
+  }
+  }
+  
+  export default Index
